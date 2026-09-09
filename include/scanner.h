8ac5569f7e70d36c500891e191c9ae69fe7c9de3
zamu-302 <samueladmaw23@gmail.com>
2026-09-09 16:13:43 +0300

    added number and identifer handling

--- include/scanner.h
+++ include/scanner.h
@@ -3,6 +3,7 @@
 #include <cstddef>
 #include <iostream>
 #include <string>
+#include <unordered_map>
 #include <variant>
 #include <vector>
 class Scanner {
@@ -19,10 +20,25 @@ private:
   int start = 0;
   int curr = 0;
   int line = 1;
+  inline static const std::unordered_map<std::string, TokenType> keywords{
+      {"and", TokenType::AND},       {"class", TokenType::CLASS},
+      {"else", TokenType::ELSE},     {"false", TokenType::FALSE},
+      {"for", TokenType::FOR},       {"fun", TokenType::FUN},
+      {"if", TokenType::IF},         {"nil", TokenType::NIL},
+      {"or", TokenType::OR},         {"print", TokenType::PRINT},
+      {"return", TokenType::RETURN}, {"super", TokenType::SUPER},
+      {"this", TokenType::THIS},     {"true", TokenType::TRUE},
+      {"var", TokenType::VAR},       {"while", TokenType::WHILE},
+  };
 
   // helpers
   char advance();
   void addToken(TokenType type);
   void addToken(TokenType type, Literal &literal);
+  void is_string();
+  char peek();
   bool match(char c);
+  char peekNext();
+  void number();
+  void identifier();
 };
--- src/scanner.cpp
+++ src/scanner.cpp
@@ -1,4 +1,6 @@
 #include "../include/scanner.h"
+#include <cctype>
+#include <string>
 
 auto Scanner::scanTokens() {
   while (!isAtEnd()) {
@@ -54,9 +56,55 @@ void Scanner::scanToken() {
   case '>':
     addToken(match('=') ? TokenType::GREATER_EQUAL : TokenType::GREATER);
     break;
-  default:
-    lox.error(line, "unexpected character");
+  case '/':
+    if (match('/')) {
+      while (peek() != '\n' && !isAtEnd()) {
+        advance();
+      }
+    }
 
+    else if (match('*')) {
+      while (!isAtEnd()) {
+        if (peek() == '*' && peekNext() == '/') {
+          advance();
+          advance();
+          break;
+        } else if (peek() == '\n') {
+          line++;
+        }
+        advance();
+      }
+
+      if (isAtEnd()) {
+        lox.error(line, "Undefined Comment Scope");
+      }
+
+    }
+
+    else {
+      addToken(TokenType::SLASH);
+    }
+    break;
+  case ' ':
+  case '\r':
+  case '\t':
+    break;
+
+  case '\n':
+    line++;
+    break;
+  case '"':
+    is_string();
+    break;
+
+  default:
+    if (std::isdigit(c)) {
+      number();
+    } else if (std::isalpha(c) || c == '_') {
+      identifer();
+    } else {
+      lox.error(line, "unexpected character");
+    }
     break;
   }
 }
@@ -70,6 +118,7 @@ void Scanner::addToken(TokenType type, Literal &l) {
   std::string text = source.substr(start, curr);
   tokens.emplace_back(Token(type, text, l, line));
 }
+void Scanner::addToken(TokenType type) { addToken(type, std::string{}); }
 bool Scanner::match(char c) {
   if (isAtEnd()) {
     return false;
@@ -79,4 +128,56 @@ bool Scanner::match(char c) {
   }
   curr++;
   return true;
+}
+char Scanner::peek() {
+  if (isAtEnd()) {
+    return '\0';
+  }
+  return source[curr];
+}
+void Scanner::is_string() {
+  while (peek() != '"' && !isAtEnd()) {
+    if (peek() == '\n') {
+      line++;
+    }
+    advance();
+  }
+  if (isAtEnd()) {
+    lox.error(line, "Unterminated String.");
+    return;
+  }
+  advance();
+  std::string value = source.substr(start + 1, curr - 1);
+  addToken(TokenType::STRING, value);
+}
+
+void Scanner::number() {
+  while (std::isdigit(peek())) {
+    advance();
+  }
+  if (peek() == '.' && std::isdigit(peekNext())) {
+    advance();
+    while (std::isdigit(peek())) {
+      advance();
+    }
+    addToken(TokenType::NUMBER, std::stod(source.substr(start, curr)));
+  }
+}
+char Scanner::peekNext() {
+  if (curr + 1 >= source.length()) {
+    return '\0';
+  }
+  return source[curr + 1];
+}
+void Scanner::identifier() {
+  while (std::isalnum(peek()) || peek() == '_') {
+    advance();
+  }
+  std::string text = source.substr(start, curr);
+  TokenType type = TokenType::IDENTFIERS;
+
+  if (keywords.contains(text)) {
+    type = keywords.at(text);
+  }
+  addToken(type);
 }
