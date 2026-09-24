26a1d51caa164cfbcfd7f37aca7e4930fe1f8e30
zamu-302 <samueladmaw23@gmail.com>
2026-09-24 22:18:18 +0300

    adding Runtime Error handling

--- include/interpreter.h
+++ include/interpreter.h
@@ -10,19 +10,60 @@ public:
     return evaluate(expr.expression);
   }
   std::any visitUnary(const Unary &expr) override {
+    std::any right = evaluate(expr.right);
     switch (expr.opr.getType()) {
     case TokenType::MINUS:
-      return -1 * std::stod(expr.right);
+      return -1 * std::any_cast<double>(right);
     case TokenType::BANG:
-      return !isTruly(expr.right);
-
-      return NULL;
+      return !isTruly(right);
     }
+    return NULL;
+  }
+  std::any visitBinary(const Binary &expr) override {
+    std::any left = evaluate(expr.left);
+    std::any right = evaluate(expr.right);
+
+    switch (expr.opr.getType()) {
+    case TokenType::MINUS:
+      return std::any_cast<double>(left) - std::any_cast<double>(right);
+
+    case TokenType::PLUS:
+      if ((typeid(double) == left.type()) && (typeid(double) == right.type())) {
+        return std::any_cast<double>(left) + std::any_cast<double>(right);
+      }
+
+      if ((typeid(std::string) == left.type()) &&
+          (typeid(std::string) == right.type())) {
+        return std::any_cast<std::string>(left) +
+               std::any_cast<std::string>(right);
+      }
+      break;
+
+    case TokenType::SLASH:
+      return std::any_cast<double>(left) / std::any_cast<double>(right);
+
+    case TokenType::STAR:
+      return std::any_cast<double>(left) * std::any_cast<double>(right);
+    case TokenType::GREATER:
+      return std::any_cast<double>(left) > std::any_cast<double>(right);
+    case TokenType::GREATER_EQUAL:
+      return std::any_cast<double>(left) >= std::any_cast<double>(right);
+    case TokenType::LESS:
+      return std::any_cast<double>(left) < std::any_cast<double>(right);
+    case TokenType::LESS_EQUAL:
+      return std::any_cast<double>(left) <= std::any_cast<double>(right);
+    case TokenType::BANG_EQUAL:
+      return !isEqual(left, right);
+    case TokenType::EQUAL_EQUAL:
+      return isEqual(left, right);
+    }
+    return NULL;
   }
 
 private:
   std::any evaluate(const std::unique_ptr<Expr> &expr) {
     return expr->accept(*this);
   }
-  bool isTruly(std::unique_ptr<Expr> expr);
+  bool isTruly(std::any expr);
+  bool isEqual(std::any left, std::any right);
 };
---
+++ src/interpreter.cpp
@@ -0,0 +1,35 @@
+#include "../include/interpreter.h"
+#include <any>
+
+bool Interpreter::isTruly(std::any obj) {
+  if (!obj.has_value()) {
+    return false;
+  }
+  if (obj.type() == typeid(bool)) {
+    return std::any_cast<bool>(obj);
+  }
+  return true;
+}
+
+bool Interpreter::isEqual(std::any left, std::any right) {
+  if (!left.has_value() && !right.has_value()) {
+    return true;
+  }
+  if (!left.has_value() || !right.has_value()) {
+    return false;
+  }
+  if (left.type() != right.type()) {
+    return false;
+  }
+  if (left.type() == typeid(double))
+    return std::any_cast<double>(left) == std::any_cast<double>(right);
+
+  if (left.type() == typeid(bool))
+    return std::any_cast<bool>(left) == std::any_cast<bool>(right);
+
+  if (left.type() == typeid(std::string))
+    return std::any_cast<std::string>(left) ==
+           std::any_cast<std::string>(right);
+
+  return false;
+}
